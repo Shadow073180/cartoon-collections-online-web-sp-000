0d98da456a949faf9d5bf2301d589f75345fae9d
David masterson <david73180@gmail.com>
2018-10-07 05:12:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,46 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  i = 0
+  dwarves = []
+  while i < array.length
+    array.each do |x|
+      puts "#{i + 1}. #{x}"
+  i += 1
+    end
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(calls)
+  array = []
+  calls.each do |x|
+    array << x.capitalize! + "!"
+    puts x
+  end
+  return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  i = 0
+  longer = []
+  shorter = []
+    while i < calls.length
+      calls.each do |x|
+        if x.length > 4
+          longer << x
+        end
+        if x.length < 4
+          shorter << x
+  i += 1
+        end
+      end
+  if shorter.length > 1 || longer.length > 1
+    return true
+  end
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
